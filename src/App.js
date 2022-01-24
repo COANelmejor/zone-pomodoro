5f7ce8a99b0c2b3cb6c8ea9d29424f5120c0f61d
Christian Arana <christian.arana@wotdev.com>
2022-01-24 14:53:36 -0600

    Added timers to the page for testing

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
+import WorkTimer from './components/WorkTimer';
+import CountdownTimer from './components/CountdownTimer';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <WorkTimer />
+      <CountdownTimer time={new Date() + 5000} />
     </div>
   );
 }
