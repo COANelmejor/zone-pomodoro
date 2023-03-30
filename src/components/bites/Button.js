0e140f3c0709ff00e7d201545d7f18b7f318e768
Christian Obed Arana navas <COANelmejor@gmail.com>
2023-03-29 22:57:14 -0600

    Added a new color for Button

--- src/components/bites/Button.js
+++ src/components/bites/Button.js
@@ -12,6 +12,10 @@ function Button({ onClickFn, onClickParams, text, color }) {
       classNameValue =
         "bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded m-2 text-2xl";
       break;
+    case "blue":
+      classNameValue =
+        "bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded m-2 text-2xl";
+      break;
     default:
       classNameValue =
         "bg-red-500 hover:bg-red-700 text-white text-white font-bold py-2 px-4 rounded m-2 text-2xl";
