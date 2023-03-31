834ebe8acaa20c28e31eb0cb5425b42b7b898d2a
Christian Obed Arana navas <COANelmejor@gmail.com>
2023-03-30 23:16:28 -0600

    Solved all comments for #5 - Installed a uuid generator for list of buutons - #discussion_r1154007142 - #discussion_r1154007206 - #discussion_r1154007257 - #discussion_r1154008137

--- src/components/cards/RestTimerSetup.js
+++ src/components/cards/RestTimerSetup.js
@@ -1,22 +1,34 @@
 
 import React from 'react';
-
+import uuid from "react-native-uuid";
 import Button from '../bites/Button';
 
 import CronometerTimeView from '../../utils/CronometerTimeView';
 
 function RestTimerSetup({ restTime, setRestTimeButton }) {
+  const buttonValues = [
+    { value: 1, text: '0.1s' },
+    { value: 10, text: '1s' },
+    { value: 100, text: '10s' },
+    { value: 600, text: '1m' },
+    { value: 6000, text: '10m' },
+    { value: 36000, text: '1h' },
+  ];
+  
   return (
     <div className="RestTime">
       <h3 className="text-3xl font-bold p-2">Rest Time</h3>
       <div className="flex justify-center items-center">
         <div className="flex flex-col col-25">
-          <Button onClickFn={setRestTimeButton} onClickParams={-1} text="-0.1s" color="red" />
-          <Button onClickFn={setRestTimeButton} onClickParams={-10} text="-1s" color="red" />
-          <Button onClickFn={setRestTimeButton} onClickParams={-100} text="-10s" color="red" />
-          <Button onClickFn={setRestTimeButton} onClickParams={-600} text="-1m" color="red" />
-          <Button onClickFn={setRestTimeButton} onClickParams={-6000} text="-10m" color="red" />
-          <Button onClickFn={setRestTimeButton} onClickParams={-36000} text="-1h" color="red" />
+          {buttonValues.map((buttonValue) => (
+            <Button
+              key={uuid.v4()}
+              onClickFn={setRestTimeButton}
+              onClickParams={-buttonValue.value}
+              text={`-${buttonValue.text}`}
+              color="red"
+            />
+          ))}
         </div>
         <div className="flex flex-col col-50">
           <h3 className="text-5xl font-bold p-2">
@@ -24,12 +36,15 @@ function RestTimerSetup({ restTime, setRestTimeButton }) {
           </h3>
         </div>
         <div className="flex flex-col col-25">
-          <Button onClickFn={setRestTimeButton} onClickParams={1} text="+0.1s" color="green" />
-          <Button onClickFn={setRestTimeButton} onClickParams={10} text="+1s" color="green" />
-          <Button onClickFn={setRestTimeButton} onClickParams={100} text="+10s" color="green" />
-          <Button onClickFn={setRestTimeButton} onClickParams={600} text="+1m" color="green" />
-          <Button onClickFn={setRestTimeButton} onClickParams={6000} text="+10m" color="green" />
-          <Button onClickFn={setRestTimeButton} onClickParams={36000} text="+1h" color="green" />
+          {buttonValues.map((buttonValue) => (
+            <Button
+              key={uuid.v4()}
+              onClickFn={setRestTimeButton}
+              onClickParams={buttonValue.value}
+              text={`+${buttonValue.text}`}
+              color="green"
+            />
+          ))}
         </div>
       </div>
     </div>
--- src/components/cards/WorkTimerSetup.js
+++ src/components/cards/WorkTimerSetup.js
@@ -1,20 +1,32 @@
 import React from "react";
+import uuid from "react-native-uuid";
 
 import Button from "../../components/bites/Button";
 import CronometerTimeView from "../../utils/CronometerTimeView";
 
 function WorkTimerSetup({ setWorkTimeButton, workTime }) {
+  const buttonValues = [
+    { value: 1, text: "0.1s" },
+    { value: 10, text: "1s" },
+    { value: 100, text: "10s" },
+    { value: 600, text: "1m" },
+    { value: 6000, text: "10m" },
+    { value: 36000, text: "1h" },
+  ];
   return (
     <div className="WorkTime">
       <h3 className="text-3xl font-bold p-2">Work Time</h3>
       <div className="flex justify-center items-center">
         <div className="flex flex-col col-25">
-          <Button onClickFn={setWorkTimeButton} onClickParams={-1} text="-0.1s" color="red" />
-          <Button onClickFn={setWorkTimeButton} onClickParams={-10} text="-1s" color="red" />
-          <Button onClickFn={setWorkTimeButton} onClickParams={-100} text="-10s" color="red" />
-          <Button onClickFn={setWorkTimeButton} onClickParams={-600} text="-1m" color="red" />
-          <Button onClickFn={setWorkTimeButton} onClickParams={-6000} text="-10m" color="red" />
-          <Button onClickFn={setWorkTimeButton} onClickParams={-36000} text="-1h" color="red" />        
+          {buttonValues.map((buttonValue) => (
+            <Button
+              key={uuid.v4()}
+              onClickFn={setWorkTimeButton}
+              onClickParams={-buttonValue.value}
+              text={`-${buttonValue.text}`}
+              color="red"
+            />
+          ))}
         </div>
         <div className="flex flex-col col-50">
           <h3 className="text-5xl font-bold p-2">
@@ -22,12 +34,15 @@ function WorkTimerSetup({ setWorkTimeButton, workTime }) {
           </h3>
         </div>
         <div className="flex flex-col col-25">
-          <Button onClickFn={setWorkTimeButton} onClickParams={1} text="+0.1s" color="green" />
-          <Button onClickFn={setWorkTimeButton} onClickParams={10} text="+1s" color="green" />
-          <Button onClickFn={setWorkTimeButton} onClickParams={100} text="+10s" color="green" />
-          <Button onClickFn={setWorkTimeButton} onClickParams={600} text="+1m" color="green" />
-          <Button onClickFn={setWorkTimeButton} onClickParams={6000} text="+10m" color="green" />
-          <Button onClickFn={setWorkTimeButton} onClickParams={36000} text="+1h" color="green" />
+          {buttonValues.map((buttonValue) => (
+            <Button
+              key={uuid.v4()}
+              onClickFn={setWorkTimeButton}
+              onClickParams={buttonValue.value}
+              text={`+${buttonValue.text}`}
+              color="green"
+            />
+          ))}
         </div>
       </div>
     </div>
